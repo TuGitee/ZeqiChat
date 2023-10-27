cae0fcfdab5b71e4feb27b6e0d3ec4ce3b6d6fbd
TuGitee <tgb@std.uestc.edu.cn>
2023-10-27 13:43:50 +0800

    vercel.json

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const JWT = require('./utils/JWT');
 const cors = require('koa2-cors');
 const fs = require('fs')
 
-const http = require('http')
+// const http = require('http')
 
-// const https = require('https')
-// const sslify = require('koa-sslify').default
+const https = require('https')
+const sslify = require('koa-sslify').default
 
 // const options = {
 //  key: fs.readFileSync('./config/zeqichat.xyz.key'),
 //  cert: fs.readFileSync('./config/zeqichat.xyz.pem'),
 // }
-// const server = https.createServer(options, app.callback());
+const server = https.createServer(options, app.callback());
 
-const server = http.createServer({}, app.callback());
+// const server = http.createServer({}, app.callback());
 
 // const server = https.createServer({}, app.callback());
 
